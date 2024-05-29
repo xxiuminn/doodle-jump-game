6dda4bcbd76f276f224b70c12f82943afc805f92
xxiuminn <xiumin1997@gmail.com>
2024-05-30 00:30:58 +0800

    added images

--- script.js
+++ script.js
@@ -1,26 +1,124 @@
-const character = document.querySelector(".character");
-let charXPos = 45;
-let charYPos = 85;
-character.style.left = String(charXPos) + "%";
-character.style.top = String(charYPos) + "%";
+// definining initial state of screen
+document.querySelector(".screen").style.width = "1280px";
+const screenWidth = parseInt(document.querySelector(".screen").style.width);
+document.querySelector(".screen").style.height = "720px";
+const screenHeight = parseInt(document.querySelector(".screen").style.height);
 
-const platforms = document.querySelectorAll(".platform");
-for (let platform of platforms) {
-  platform.style.left = String(Math.floor(Math.random() * 67)) + "%";
-  console.log(platform.style.left);
-}
-
+//score
 let score = 0;
 
-// let charPos = [charXPos, charYPos]; //["45%,"95%"]
+//creating platforms
+let numOfPlat = 7;
+let platformArr = [];
+for (let i = 0; i < numOfPlat; i++) {
+  const platform = document.createElement("div");
+  document.querySelector(".screen").appendChild(platform);
+  platform.classList.add("platform");
+  platform.style.top = String((100 / (numOfPlat + 1)) * (i + 1)) + "%";
+  platform.style.left = String(Math.random() * (screenWidth - 120)) + "px";
+  platformArr.push(platform);
+}
 
-// const platform = document.querySelector(".platform");
-// let platXPos = "20%";
-// let platYPos = "70%";
-// platform.style.left = platXPos;
-// platform.style.top = platYPos;
-// platform.style.width = "20%";
-// let platPos = [platXPos, platYPos]; //["20%","70%"]
+//creating character
+const character = document.querySelector(".character");
+character.style.top =
+  String((parseInt(platformArr[6].style.top) / 100) * screenHeight - 82) + "px";
+character.style.left = String(parseInt(platformArr[6].style.left) + 14) + "px";
+charLeft = parseInt(character.style.left);
+charTop = parseInt(character.style.top);
+console.log(charTop);
+
+//jumps
+function isJump() {
+  character.classList.add("isJumping");
+  character.style.top = "60%";
+  // charTop = character.style.top;
+  // if (checkCharXPos()) {
+  //   land();
+  // landed();
+  // }
+  setTimeout(() => character.classList.remove("isJumping"), 700);
+}
+
+// function landed() {
+//   character.style.top = String((100 / (numOfPlat + 1)) * 6) + "%";
+//   charTop = character.style.top;
+// }
+
+// function land() {
+//   let platTop = parseInt(platformContainer.style.top);
+//   platTop += 25;
+//   console.log(platTop);
+//   platformContainer.style.top = String(platTop) + "%";
+// }
+
+//keyboard controls
+document.addEventListener("keydown", (e) => {
+  if (e.key == " ") {
+    isJump();
+  } else if (e.key === "ArrowRight") {
+    moveRight();
+    document.querySelector(".character").style.backgroundImage =
+      "var(--doodle-right-image)";
+  } else if (e.key === "ArrowLeft") {
+    moveLeft();
+    document.querySelector(".character").style.backgroundImage =
+      "var(--doodle-left-image)";
+  }
+});
+
+function moveRight() {
+  charLeft += 100;
+  character.style.left = String(charLeft) + "px";
+}
+function moveLeft() {
+  charLeft -= 100;
+  character.style.left = String(charLeft) + "px";
+}
+
+// const platformContainer = document.querySelector(".platformcontainer");
+// platformContainer.style.top = "0%";
+
+// randomise order of the platforms
+// const platforms = document.querySelectorAll(".platform");
+// for (let platform of platforms) {
+//   platform.style.left = String(Math.floor(Math.random() * 67)) + "%";
+// }
+
+//identifying the next platform character should be on.
+// let nextPlatLeft = platforms[1].offsetLeft;
+
+//check if character will land on next platform
+
+// const checkCharXPos = () => {
+//   console.log(parseInt(character.style.left));
+//   console.log(nextPlatLeft / 10);
+//   console.log(nextPlatLeft / 10 - 10);
+//   console.log(nextPlatLeft / 10 + 33 + 10);
+//   if (
+//     parseInt(character.style.left) > nextPlatLeft / 10 - 10 &&
+//     parseInt(character.style.left) < nextPlatLeft / 10 + 33
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// add & remove platforms
+// const addPlat = () => {
+//   let div1 = document.createElement("div");
+//   div1.classList.add("row");
+//   document.querySelector(".platformcontainer").prepend(div1);
+
+//   let div2 = document.createElement("div");
+//   div2.classList.add("platform");
+//   document.querySelector(".row").appendChild(div2);
+// };
+
+// const removePlat = () => {
+//   document.querySelector;
+// };
 
 // class Createplatform {
 //   constructor(platNum, platXPos, platYPos) {
@@ -30,51 +128,25 @@ let score = 0;
 //   }
 // }
 
-document.addEventListener("keydown", (e) => {
-  if (e.key == " ") {
-    isJump();
-  } else if (e.key === "ArrowRight") {
-    moveRight();
-  } else if (e.key === "ArrowLeft") {
-    moveLeft();
-  }
-});
-
-function isJump() {
-  console.log(charYPos);
-  charYPos = charYPos - 27.5;
-  console.log((character.style.top = String(charYPos) + "%"));
-  // character.style.transform = "translateY(-250px)";
-  // setTimeout(() => {
-  //   character.style.transform = "translateY(0px)";
-  // }, 150);
-  //   character.classList.add("isJumping");
-  //   setTimeout(() => character.classList.remove("isJumping"), 500);
-}
-console.log((character.style.top = String(charYPos) + "%"));
-
-// setInterval((landed) => {
-//   let charYMaxPos =
-//     String(
-//       parseInt(window.getComputedStyle(character).getPropertyValue("top")) / 10
-//     ) + "%";
+// setInterval((checkCharXPos) => {
 //   if (
-//     charYMaxPos === platYPos &&
-//     parseInt(charXPos) >= parseInt(platXPos) &&
-//     parseInt(charXPos) <= parseInt(platXPos) + parseInt(platform.style.width)
+//     parseInt(character.style.left) >= nextPlatLeft / 10 &&
+//     parseInt(character.style.left) <= nextPlatLeft / 10 + 33
 //   ) {
+//     // console.log(true);
 //     return true;
-//   } else false;
+//   } else {
+//     return false;
+//     // console.log(false);
+//   }
 // }, 1000);
 
-function moveRight() {
-  let newCharXPos = charXPos + 20;
-  charXPos = newCharXPos;
-  character.style.left = String(charXPos) + "%";
-}
+// const addPlat = () => {
+//   let div1 = document.createElement("div");
+//   div1.classList.add("row");
+//   document.querySelector(".platformcontainer").prepend(div1);
 
-function moveLeft() {
-  let newCharXPos = charXPos - 20;
-  charXPos = newCharXPos;
-  character.style.left = String(charXPos) + "%";
-}
+//   let div2 = document.createElement("div");
+//   div2.classList.add("platform");
+//   document.querySelector(".row").appendChild(div2);
+// };
